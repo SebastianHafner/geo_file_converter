6fc8c547facf397c54ece575e9cffa7fe524e6d4
SebastianHafner <hafnersailing@gmail.com>
2021-04-08 14:22:13 +0200

    conversion of all geojson files in a folder is now supported

--- geojson2shp.py
+++ geojson2shp.py
@@ -21,13 +21,11 @@ def argument_parser():
     return parser
 
 
-def convert_geojson2shp(args):
-    geojson_file = Path(args.geojson_file)
+def convert_geojson2shp(geojson_file: Path, output_path: Path = None):
     if not geojson_file.exists():
         raise Exception(f'file {geojson_file} does not exist')
 
-    output_path = args.shape_file
-    output_path = Path(output_path) if not output_path == 'input' else geojson_file.parent
+    output_path = geojson_file.parent if output_path is None else output_path
     if not output_path.exists():
         raise Exception(f'directory {output_path} does not exist')
 
@@ -36,12 +34,27 @@ def convert_geojson2shp(args):
         output_path.mkdir()
 
     print(f'reading in {geojson_file}...')
-    gdf = gpd.read_file(geojson_file)
+    gdf = gpd.read_file(str(geojson_file))
 
     print(f'saving to {output_path}...')
     shape_file = output_path / f'{geojson_file.stem}.shp'
-    gdf.to_file(shape_file)
+    gdf.to_file(str(shape_file))
     print(f'done')
+
+
+def run_geojson2shp_converter(args):
+    geojson_file = Path(args.geojson_file)
+    output_path = args.shape_file
+
+    if geojson_file.is_dir():
+        directory = geojson_file
+        print(f'converting all files in directory {directory}')
+        geojson_files = [f for f in directory.iterdir() if f.is_file()]
+        for geojson_file in geojson_files:
+            convert_geojson2shp(geojson_file)
+    else:
+        output_path = Path(output_path) if not output_path == 'input' else geojson_file.parent
+        convert_geojson2shp(geojson_file, output_path)
 
 
 if __name__ == '__main__':
@@ -49,4 +62,4 @@ if __name__ == '__main__':
     parser = argument_parser()
     args = parser.parse_known_args()[0]
 
-    convert_geojson2shp(args)
+    run_geojson2shp_converter(args)
